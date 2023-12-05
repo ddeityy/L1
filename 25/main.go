588752076f759f0d500b1f8674c41f19a21e00a2
ddeity <ddeity@gmail.com>
2023-12-05 22:37:45 +0300

    25, 26

--- 25/main.go
+++ 25/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+/*
+Реализовать собственную функцию sleep.
+*/
+
+func sleep(duration time.Duration) {
+	<-time.After(duration)
+}
+
+func main() {
+	start := time.Now()
+	sleep(5 * time.Second)
+	fmt.Println(time.Since(start))
 }
--- 26/main.go
+++ 26/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+/*
+Разработать программу, которая проверяет, что все символы в строке уникальные
+(true — если уникальные, false etc). Функция проверки должна быть регистронезависимой.
+
+Например:
+abcd — true
+abCdefAaf — false
+aabcd — false
+*/
+
+func allUnique(str string) bool {
+	str = strings.ToLower(str)
+
+	runeSlice := []rune(str)
+	set := make(map[string]interface{})
+
+	for _, char := range runeSlice {
+		if _, ok := set[string(char)]; ok {
+			continue
+		}
+		set[string(char)] = struct{}{}
+	}
+
+	return len(runeSlice) == len(set)
+}
+
+func main() {
+	fmt.Println(allUnique("abcd"))
+	fmt.Println(allUnique("abCdefAaf"))
+	fmt.Println(allUnique("aabcd"))
 }
